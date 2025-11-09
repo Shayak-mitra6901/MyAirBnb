efe59e3180f8c629fdc1f6013be5352bd85cdbf8
Shayak.mitra <shayakmitra6901@gmail.com>
2025-11-09 14:52:40 +0530

    AirBnb backend project using spring boot

---
+++ src/main/java/com/codingshuttle/projects/airBnbApp/controller/HotelBookingController.java
@@ -0,0 +1,48 @@
+package com.codingshuttle.projects.airBnbApp.controller;
+
+import com.codingshuttle.projects.airBnbApp.dto.BookingDto;
+import com.codingshuttle.projects.airBnbApp.dto.BookingRequest;
+import com.codingshuttle.projects.airBnbApp.dto.GuestDto;
+import com.codingshuttle.projects.airBnbApp.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bookings")
+public class HotelBookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping("/init")
+    public ResponseEntity<BookingDto> initialiseBooking(@RequestBody BookingRequest bookingRequest) {
+        return ResponseEntity.ok(bookingService.initialiseBooking(bookingRequest));
+    }
+
+    @PostMapping("/{bookingId}/addGuests")
+    public ResponseEntity<BookingDto> addGuests(@PathVariable Long bookingId,
+                                                @RequestBody List<GuestDto> guestDtoList) {
+        return ResponseEntity.ok(bookingService.addGuests(bookingId, guestDtoList));
+    }
+
+    @PostMapping("/{bookingId}/payments")
+    public ResponseEntity<Map<String, String>> initiatePayment(@PathVariable Long bookingId) {
+        String sessionUrl = bookingService.initiatePayments(bookingId);
+        return ResponseEntity.ok(Map.of("sessionUrl", sessionUrl));
+    }
+
+    @PostMapping("/{bookingId}/cancel")
+    public ResponseEntity<Void> cancelBooking(@PathVariable Long bookingId) {
+        bookingService.cancelBooking(bookingId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{bookingId}/status")
+    public ResponseEntity<Map<String, String>> getBookingStatus(@PathVariable Long bookingId) {
+        return ResponseEntity.ok(Map.of("status", bookingService.getBookingStatus(bookingId)));
+    }
+}
